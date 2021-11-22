27cde7fef5048f7007350384b4fa3ad8dd9e0621
Elliott7 <elliottbreadmore@hotmail.com>
2021-11-22 16:00:11 +1000

    Added involved scroll bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+let progress = document.getElementById('progressBar')
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function (){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100
+    progress.style.height = `${progressHeight}%`
+}
