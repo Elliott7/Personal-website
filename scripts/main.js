32115e1eb8bd43b62d95f49c971947ba4d643eeb
Elliott7 <elliottbreadmore@hotmail.com>
2021-11-23 14:29:06 +1000

    Added further projects sections, cleaned up and improved responsiveness, improved animations on load

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,38 @@
+
+// Particles effects
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
 
+// Scroll bar effect
 let progress = document.getElementById('progressBar')
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function (){
     let progressHeight = (window.pageYOffset / totalHeight) * 100
     progress.style.height = `${progressHeight}%`
 }
+
+// Observer API used to add animations once certain elements are within the viewport
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+}
+let observer = new IntersectionObserver(callbackFunc, options)
+observer.observe(document.getElementById('my-description'))
+
+function callbackFunc(entries){
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            let items = document.getElementsByClassName('skills-item')
+            let text = document.getElementById('my-description')
+            for (let i = 0; i < items.length; i++){
+                items[i].classList.add('skills-item-ani')
+            }
+            text.classList.add('about-me-info-ani')
+        }
+        let txt = `${entry.target.id} visibility ${entry.isIntersecting}`
+        console.log(txt)
+    })
+}
+
